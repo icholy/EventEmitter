93657de007f4c73938124884fb29c8fa2929d9de
Ilia Choly <icholy@accipiterradar.com>
2016-01-14 10:40:53 -0500

    rebuild

--- build/EventEmitter.d.ts
+++ build/EventEmitter.d.ts
@@ -1,7 +1,30 @@
 declare type EventEmitterCallback = (payload?: any) => any;
 declare class EventEmitter<E> {
     private _channels;
+    /**
+     * Add an event listener
+     *
+     * @param name The event to subscribe to
+     * @param callback The callback function to invoke
+     * @return unbind function
+     */
     on(name: E, callback: EventEmitterCallback): (...any) => any;
+    /**
+     * Remove an event listener
+     *
+     * @param name The event to unsubscribe from
+     * @param callback The callback to unsubscribe
+     */
     off(name: E, callback: EventEmitterCallback): void;
+    /**
+     * Remove all event listeners
+     */
+    reset(): void;
+    /**
+     * Emit an event
+     *
+     * @param name The event type
+     * @param payload Optional data passed to listeners
+     */
     emit(name: E, payload?: any): void;
 }
