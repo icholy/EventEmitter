fbdf85ac0de30373b478d683cede35d824d9f2d1
Ilia Choly <icholy@accipiterradar.com>
2016-01-14 10:19:37 -0500

    rebuild

--- build/EventEmitter.d.ts
+++ build/EventEmitter.d.ts
@@ -1,7 +1,7 @@
 declare type EventEmitterCallback = (payload?: any) => any;
 declare class EventEmitter<E> {
     private _channels;
-    on(name: E, callback: EventEmitterCallback): Function;
+    on(name: E, callback: EventEmitterCallback): (...any) => any;
     off(name: E, callback: EventEmitterCallback): void;
     emit(name: E, payload?: any): void;
 }
