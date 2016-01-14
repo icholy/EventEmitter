de648d3845c7cde9b2807f262a9f7b39cc8465cd
Ilia Choly <icholy@accipiterradar.com>
2016-01-14 11:45:30 -0500

    rename methods

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -16,15 +16,15 @@ class EventEmitterGroup<E> {
    * @param name The event to subscribe to 
    * @param callback The callback function to invoke
    */
-  on(name: E, callback: EventEmitterCallback): void {
-    let unbind = this._emitter.on(name, callback);
+  addEventListener(name: E, callback: EventEmitterCallback): void {
+    let unbind = this._emitter.addEventListener(name, callback);
     this._unbind.push(unbind);
   }
 
   /**
    * Remove all event listeners in group
    */
-  off(): void {
+  removeEventListeners(): void {
     this._unbind.forEach((unbind) => {
       unbind();
     });
@@ -34,7 +34,7 @@ class EventEmitterGroup<E> {
 
 class EventEmitter<E> {
 
-  private _channels: { [name:number]: EventEmitterCallback[]; } = {};
+  private _eventEmitterChannels: { [name:number]: EventEmitterCallback[]; } = {};
 
   /**
    * Add an event listener
@@ -42,13 +42,13 @@ class EventEmitter<E> {
    * @param name The event to subscribe to @param callback The callback function to invoke
    * @return unbind function
    */
-  on(name: E, callback: EventEmitterCallback): (...any) => any {
-    if (!this._channels.hasOwnProperty(<any>name)) {
-      this._channels[<any>name] = [callback];
+  addEventListener(name: E, callback: EventEmitterCallback): (...any) => any {
+    if (!this._eventEmitterChannels.hasOwnProperty(<any>name)) {
+      this._eventEmitterChannels[<any>name] = [callback];
     } else {
-      this._channels[<any>name].push(callback);
+      this._eventEmitterChannels[<any>name].push(callback);
     }
-    return () => this.off(name, callback);
+    return () => this.removeEventListener(name, callback);
   }
 
   /**
@@ -57,8 +57,8 @@ class EventEmitter<E> {
    * @param name The event to unsubscribe from
    * @param callback The callback to unsubscribe
    */
-  off(name: E, callback: EventEmitterCallback): void {
-    let channels = this._channels;
+  removeEventListener(name: E, callback: EventEmitterCallback): void {
+    let channels = this._eventEmitterChannels;
     if (channels.hasOwnProperty(<any>name)) {
       let channel = channels[<any>name];
       let index = channel.indexOf(callback);
@@ -73,11 +73,11 @@ class EventEmitter<E> {
    *
    * @param name Event to reset (defaults to all)
    */
-  reset(name?: E): void {
+  removeEventListeners(name?: E): void {
     if (typeof name === 'undefined') {
-      this._channels = {};
-    } else if (this._channels.hasOwnProperty(<any>name)) {
-      delete this._channels[<any>name];
+      this._eventEmitterChannels = {};
+    } else if (this._eventEmitterChannels.hasOwnProperty(<any>name)) {
+      delete this._eventEmitterChannels[<any>name];
     }
   }
 
@@ -87,9 +87,9 @@ class EventEmitter<E> {
    * @param name The event type
    * @param payload Optional data passed to listeners
    */
-  emit(name: E, payload?: any): void {
-    if (this._channels.hasOwnProperty(<any>name)) {
-      this._channels[<any>name].forEach((callback) => {
+  emitEvent(name: E, payload?: any): void {
+    if (this._eventEmitterChannels.hasOwnProperty(<any>name)) {
+      this._eventEmitterChannels[<any>name].forEach((callback) => {
         callback(payload);
       });
     }
@@ -100,7 +100,7 @@ class EventEmitter<E> {
    *
    * @return a group of listeners
    */
-  group(): EventEmitterGroup<E> {
+  createEventGroup(): EventEmitterGroup<E> {
     return new EventEmitterGroup(this);
   }
 
