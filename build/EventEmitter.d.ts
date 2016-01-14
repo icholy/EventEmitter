87139297a708e1c318d26e50d8bc5c7e3153ecff
Ilia Choly <icholy@accipiterradar.com>
2016-01-14 11:45:36 -0500

    rebuild

--- build/EventEmitter.d.ts
+++ build/EventEmitter.d.ts
@@ -9,45 +9,45 @@ declare class EventEmitterGroup<E> {
      * @param name The event to subscribe to
      * @param callback The callback function to invoke
      */
-    on(name: E, callback: EventEmitterCallback): void;
+    addEventListener(name: E, callback: EventEmitterCallback): void;
     /**
      * Remove all event listeners in group
      */
-    off(): void;
+    removeEventListeners(): void;
 }
 declare class EventEmitter<E> {
-    private _channels;
+    private _eventEmitterChannels;
     /**
      * Add an event listener
      *
      * @param name The event to subscribe to @param callback The callback function to invoke
      * @return unbind function
      */
-    on(name: E, callback: EventEmitterCallback): (...any) => any;
+    addEventListener(name: E, callback: EventEmitterCallback): (...any) => any;
     /**
      * Remove an event listener
      *
      * @param name The event to unsubscribe from
      * @param callback The callback to unsubscribe
      */
-    off(name: E, callback: EventEmitterCallback): void;
+    removeEventListener(name: E, callback: EventEmitterCallback): void;
     /**
      * Remove all event listeners
      *
      * @param name Event to reset (defaults to all)
      */
-    reset(name?: E): void;
+    removeEventListeners(name?: E): void;
     /**
      * Emit an event
      *
      * @param name The event type
      * @param payload Optional data passed to listeners
      */
-    emit(name: E, payload?: any): void;
+    emitEvent(name: E, payload?: any): void;
     /**
      * Create a group of listeners that can be unbound all together
      *
      * @return a group of listeners
      */
-    group(): EventEmitterGroup<E>;
+    createEventGroup(): EventEmitterGroup<E>;
 }
